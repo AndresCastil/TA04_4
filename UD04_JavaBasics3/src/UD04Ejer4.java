2d06495d5c36f5adc304281e5f614fe0e740ee71
AndresCastil <casaned@yahoo.com>
2022-10-03 02:06:05 +0200

    Primer commit

---
+++ UD04_JavaBasics3/src/UD04Ejer4.java
@@ -0,0 +1,19 @@
+
+public class UD04Ejer4 {
+	public static void main(String[] args){
+		
+		int N=21;
+		
+		System.out.println("Valor inicial de N = "+N);
+		System.out.println("N + 77 "+ "= "+  (N=N+77));
+		System.out.println("N - 3 "+ "= "+ (N=N-3));
+		System.out.println("N * 2 "+ "= "+ (N=N*2));
+		
+		
+		
+		
+	
+	}
+	
+
+}
